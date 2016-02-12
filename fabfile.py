e803c036f523f37bcc593a5e0ffc5461f073db0c
Matthew Hanson <matt.a.hanson@gmail.com>
2016-02-12 12:57:49 -0500

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,24 @@
-import os
 from fabric.operations import run, put, sudo
 
-def deploy():
-    put('.env', '~/', use_sudo=True)
-    put('docker-compose.yml', '~/', use_sudo=True)
-    put('osm-stats-api', '~/', use_sudo=True)
-    put('planet-stream', '~/', use_sudo=True)
+
+def setup_host():
+    """ Update machine and install required packages """
     sudo('yum update -y')
     sudo('yum install -y docker')
     sudo('pip install docker-compose')
     sudo('service docker start')
     sudo('usermod -a -G docker ec2-user')
+
+
+def copy_files():
+    """ Copy Docker files to host """
+    put('.env', '~/', use_sudo=True)
+    put('docker-compose.yml', '~/', use_sudo=True)
+    put('osm-stats-api', '~/', use_sudo=True)
+    put('planet-stream', '~/', use_sudo=True)
+
+
+def deploy():
+    """ Deploy docker containers """
     run('docker-compose build')
-    run('docker-compose up')
+    run('docker-compose up -d')
