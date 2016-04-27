8a21930c58eadfe21bceae39df30d9cadc593a7b
Marc Farra <marcfarra@gmail.com>
2016-04-27 16:40:35 -0400

    Bump planet-stream to 3.0 and add more logging

--- deploy/planet-stream/index.js
+++ deploy/planet-stream/index.js
@@ -1,23 +1,25 @@
 // OSM Planet Stream service
 // Mostly from https://github.com/developmentseed/planet-stream/tree/master/examples/kinesis
 
-console.log(process.env);
+var Log = require('log');
+var log = new Log(process.env.LOG_LEVEL || 'info');
+log.debug(process.env);
 
-var redis_host = process.env.REDIS_PORT_6379_TCP_ADDR || process.env.REDIS_HOST || '127.0.0.1'
-var redis_port = process.env.REDIS_PORT_6379_TCP_PORT || process.env.REDIS_PORT || 6379
+var redis_host = process.env.REDIS_PORT_6379_TCP_ADDR || process.env.REDIS_HOST || '127.0.0.1';
+var redis_port = process.env.REDIS_PORT_6379_TCP_PORT || process.env.REDIS_PORT || 6379;
 
-var forgettable_host = process.env.FORGETTABLE_PORT_8080_TCP_ADDR || '127.0.0.1'
-var forgettable_port = process.env.FORGETTABLE_PORT_8080_TCP_PORT || 8080
+var forgettable_host = process.env.FORGETTABLE_PORT_8080_TCP_ADDR || '127.0.0.1';
+var forgettable_port = process.env.FORGETTABLE_PORT_8080_TCP_PORT || 8080;
 
 var kinesis = require('./lib/kinesis.js');
 var R = require('ramda');
 var Redis = require('ioredis');
 var request = require('request-promise');
 
-redis = new Redis({
+var redis = new Redis({
   host: redis_host,
   port: redis_port
-})
+});
 var toGeojson = require('./lib/toGeojson.js');
 
 var tracked = ['#missingmaps'];
@@ -40,42 +42,45 @@ function addToKinesis(obj) {
   var data = JSON.stringify(obj);
   var geo = toGeojson(obj.elements);
   geo.properties = obj.metadata;
-  console.log(obj.metadata);
+  log.debug('[kinesis obj metadata]:' + obj.metadata);
 
   // Only add if there are features
   if (geo.features.length) {
     var hashtags = getHashtags(obj.metadata.comment);
     hashtags.forEach(function (hashtag) {
       redis.lpush('osmstats::map::' + hashtag, JSON.stringify(geo));
-      redis.ltrim('osmstats::map::' + hashtag, 0, 100)
+      redis.ltrim('osmstats::map::' + hashtag, 0, 100);
 
       // Add to forgettable
       if (R.match(/missingmaps/, R.toLower(hashtag)).length === 0) {
         request('http://' + forgettable_host + ':' + forgettable_port +
                      '/incr?distribution=hashtags&field=' + R.slice(1, Infinity, hashtag) + '&N=10').then(function (result) {
         }).catch(function (error) {
-          console.error('error', error)
-        })
+          console.error('error', error);
+        });
       }
     });
   }
 
-
   if (process.env.PS_OUTPUT_DEBUG) {
-    console.log(JSON.stringify(geo));
+    log.debug(JSON.stringify(geo));
   } else {
     if (obj.metadata) {
+      log.debug('About to add ' + obj.metadata.id);
       var dataParams = {
         Data: data,
         PartitionKey: obj.metadata.id.toString(),
         StreamName: process.env.KINESIS_STREAM
       };
       kinesis.kin.putRecord(dataParams, function (err, data) {
-        if (err) console.error(err);
-        else console.log(data);
+        if (err) log.error(err);
+        else {
+          log.info('Added ' + obj.metadata.id);
+          log.debug('object:' + data);
+        }
       });
     } else {
-      console.log('No metadata for ' + obj);
+      log.info('No metadata for ' + obj);
     }
   }
 }
@@ -89,13 +94,12 @@ if (process.env.SIMULATION) {
   setInterval(function () {
     var changeset = simulation.randomChangeset();
     addToKinesis(changeset);
-  }, 1000)
+  }, 1000);
 
 } else {
 
   // Start planet-stream
   var diffs = require('planet-stream')({
-    verbose: process.env.DEBUG || false,
     limit: process.env.LIMIT || 25,
     host: redis_host,
     port: redis_port
