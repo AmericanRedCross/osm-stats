3f5cf38aa91bff222b20c735de77fcee912515bf
eternaltyro <eternaltyro@protonmail.ch>
2022-11-27 14:06:48 +0000

    Fix sensitivity for outputs

--- deployment/output.tf
+++ deployment/output.tf
@@ -1,11 +1,11 @@
 output "redis_url" {
   description = "FQDN for Redis Cache"
-  sensitive   = false
+  sensitive   = true
   value       = "redis://:${urlencode(azurerm_redis_cache.osm-stats.primary_access_key)}@${azurerm_redis_cache.osm-stats.hostname}:${azurerm_redis_cache.osm-stats.port}/1"
 }
 
 output "database_url" {
   description = "PostgreSQL connection string"
-  sensitive   = false
+  sensitive   = true
   value       = "postgresql://${var.db_user}%40${var.db_server_name}:${random_string.db_password.result}@${azurerm_postgresql_server.osm-stats.fqdn}/${var.db_name}"
 }
